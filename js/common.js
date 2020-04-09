36f7d5c97ff0bff9c7d1252aa8a827d3d1f6b2c5
ITkah <ilatkacenko132@gmail.com>
2020-04-10 02:57:00 +0300

    new fix

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     $(".menu-btn").on("click", function() {
         $("nav").slideToggle(200);
     });
@@ -12,46 +12,40 @@ $(document).ready(function() {
         }
     });
 
-    function f_acc(){
+    function f_acc() {
         $('#accordeon .acc-body').not($(this).next()).slideUp(500);
         $(this).next().slideToggle(500);
     }
 
-    $(".why .acc-head").on("click", function(){
-      $(".why .acc-head").removeClass("acc-minus");
-      $(this).addClass("acc-minus");
-    });
-
     $('#accordeon .acc-head').on('click', f_acc);
-    
+
     $('.team-slider').slick({
         dots: true,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              dots: true
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
         ]
-      });
+    });
 });
