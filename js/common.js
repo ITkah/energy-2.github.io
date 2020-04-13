b4cb7e238256b85516df440446d2e3803e074863
ITkah <ilatkacenko132@gmail.com>
2020-04-13 13:34:46 +0300

    new blog

--- js/common.js
+++ js/common.js
@@ -17,10 +17,85 @@ $(document).ready(function() {
         $(this).next().slideToggle(500);
     }
 
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $(".form-fedback").submit(function() {
+        var form_data = $(this).serializeArray();
+        $.ajax({
+            type: "POST",
+            url: "../../mail.php",
+            data: form_data,
+            success: function(success) {
+                $('.popup-with-form').click();
+                console.log(success);
+            },
+            error: function(error) {
+                $('.popup-with-form').click();
+                console.log(error);
+            }
+        });
+        return false;
+    });
+
+    $('.popup-with-form').magnificPopup({
+        type: 'inline',
+        preloader: false,
+        focus: '#name',
+
+        // When elemened is focused, some mobile browsers in some cases zoom in
+        // It looks not nice, so we disable it:
+        callbacks: {
+            beforeOpen: function() {
+                if($(window).width() < 700) {
+                    this.st.focus = false;
+                } else {
+                    this.st.focus = '#name';
+                }
+            }
+        }
+    });
+
+
     $('#accordeon .acc-head').on('click', f_acc);
 
     $('.team-slider').slick({
         dots: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+    $('.blog-slider').slick({
+        dots: true,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
